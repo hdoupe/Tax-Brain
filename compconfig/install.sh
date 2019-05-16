717f2e2978041c1cba0570be32ede6d92c05cfec
hdoupe <henrymdoupe@gmail.com>
2019-05-16 16:24:38 -0400

    Update install.sh

--- compconfig/install.sh
+++ compconfig/install.sh
@@ -1,3 +1,4 @@
 # bash commands for installing your package
-conda install -c conda-forge "paramtools>=0.5.4"
+
+conda install -c conda-forge -c pslmodels "paramtools>=0.5.4" taxcalc behresp dask pytest
 pip install "git+https://github.com/hdoupe/Tax-Brain.git@tmp-install"
