6bfca2109c42754fa4827912a843c2bf559e32b7
hdoupe <henrymdoupe@gmail.com>
2020-05-26 10:21:43 -0400

    Update cs install scripts for testing on this branch

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,2 +1,8 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" "paramtools>=0.14.1" bokeh pypandoc
+
+git clone -b pt-updates --depth 1 https://github.com/hdoupe/Tax-Brain
+
+cd Tax-Brain
+
+pip install -e .
