9debe9bea2a3df78521c3c543e2abf056173530d
hdoupe <henrymdoupe@gmail.com>
2020-05-26 10:38:03 -0400

    Add behresp

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,5 +1,5 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" "paramtools>=0.14.1" bokeh pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" behresp "paramtools>=0.14.1" bokeh pypandoc
 
 git clone -b pt-updates --depth 1 https://github.com/hdoupe/Tax-Brain
 
