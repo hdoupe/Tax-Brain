fd2f6f133dcd58587f09a7ddc122dffd4caafe58
hdoupe <henrymdoupe@gmail.com>
2020-07-20 11:05:04 -0400

    Install from anderson's fork

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,2 +1,21 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxbrain>=2.3.2" "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
+
+conda install -c conda-forge pytest \
+ dask \
+ bokeh \
+ markdown \
+ cairo \
+ pango \
+ tabulate \
+ selenium \
+ pypandoc \
+ nodejs \
+ pip \
+ geckodriver \
+ firefox
+
+
+git clone --branch addreports https://github.com/andersonfrailey/Tax-Brain
+cd Tax-Brain
+pip install -e .
