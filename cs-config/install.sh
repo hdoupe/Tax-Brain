159acd6ef25e16ec6d6b928d8b8ac604f2ecf4b7
hdoupe <henrymdoupe@gmail.com>
2020-07-20 12:11:31 -0400

    Add behresp to deps

--- cs-config/install.sh
+++ cs-config/install.sh
@@ -1,5 +1,5 @@
 # bash commands for installing your package
-conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" "paramtools>=0.10.2" bokeh pypandoc
+conda install -c pslmodels -c conda-forge nodejs "taxcalc>=2.5.0" "behresp" "paramtools>=0.10.2" bokeh pypandoc
 
 conda install -c conda-forge pytest \
  dask \
